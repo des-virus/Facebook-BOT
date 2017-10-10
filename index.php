2ff97c0893763bfc5476e99838e746450931f360
D Virus <duymanhtemp@gmail.com>
2017-10-10 10:29:13 +0700

    Add index file

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
-﻿<?php 
-echo "Xin chao, day la index 123 1234";
-?>
+<!DOCTYPE html>
+<html ng-app="app">
+    <head>
+        <meta charset="UTF-8">
+        <title>Khai phá dữ liệu</title>
+
+        <link href="css/lib/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+
+        <script src="js/lib/angular.min.js" type="text/javascript"></script>
+        <script src="js/lib/jquery-3.1.1.min.js" type="text/javascript"></script>
+        <script src="js/lib/bootstrap.min.js" type="text/javascript"></script>
+
+        <script src="js/module/app.js" type="text/javascript"></script>
+        <script src="js/service/RoomService.js" type="text/javascript"></script>
+        <script src="js/controller/RoomController.js" type="text/javascript"></script>
+
+    </head>
+    <body ng-controller="RoomCtrl">
+        <?php
+            require './partial/nav.php';
+        ?>
+
+        <div class="container">
+            
+
+
+        </div>
+
+    </body>
+</html>
